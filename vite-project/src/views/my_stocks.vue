fbb5982e9bb2297507980e38d0731032e211f3f9
Javen420 <Javent2003@gmail.com>
2025-11-22 10:40:34 +0800

    changed my stocks page

--- vite-project/src/views/my_stocks.vue
+++ vite-project/src/views/my_stocks.vue
@@ -3,27 +3,49 @@ will only appear if user is logged in -->
 
 <template>
     <div id="main_container" class="container">
-        <h1 class="text-center">
-            My stocks
-        </h1>
-        <div id="individual stocks">
-            <div v-for="stock in stocks" id="indi_stock">
-                <img :src="stock.img"></img>
-                <p>
-                    {{ stock.name }}
-                </p>
-                <p>
-                    {{ stock.description }}
-                </p>
+
+        <!-- Portfolio Section -->
+        <div id="port_section">
+            <h1 class="text-center">
+                portfolios
+            </h1>
+            <div id="Portfolios">
+                <div v-for="portfolio in portfolios" id="portfolio">
+                    <img :src="portfolio.img"></img>
+                    <p>
+                        {{ portfolio.name }}
+                    </p>
+                    <p>
+                        {{ portfolio.description }}
+                    </p>
+                </div>
+            </div>
+        </div>
+
+        <!-- wishlist section -->
+        <div>
+            <h1 class="text-center">
+                Wish list
+            </h1>
+            <div id="individual stocks">
+                <div v-for="stock in wishlist" id="indi_stock">
+                    <img :src="stock.img"></img>
+                    <p>
+                        {{ stock.name }}
+                    </p>
+                    <p>
+                        {{ stock.description }}
+                    </p>
+                </div>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-
+    export default{
+        name: "my_stocks"
+    }
 </script>
 
-<style>
-    
-</style>
+<style></style>
