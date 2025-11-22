ee3270e20e3465e8872cb7e64acdfa87657c3d26
Javen420 <Javent2003@gmail.com>
2025-11-22 10:34:11 +0800

    my stocks page

--- vite-project/src/views/my_stocks.vue
+++ vite-project/src/views/my_stocks.vue
@@ -1,2 +1,29 @@
 <!-- displays all the portfolios/stocks that the user has saved.
 will only appear if user is logged in -->
+
+<template>
+    <div id="main_container" class="container">
+        <h1 class="text-center">
+            My stocks
+        </h1>
+        <div id="individual stocks">
+            <div v-for="stock in stocks" id="indi_stock">
+                <img :src="stock.img"></img>
+                <p>
+                    {{ stock.name }}
+                </p>
+                <p>
+                    {{ stock.description }}
+                </p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+    
+</style>
