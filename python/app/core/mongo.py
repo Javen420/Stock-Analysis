6c08ac147cd98de3dc1bf2b0b18d5842cdc8c19c
Javen420 <Javent2003@gmail.com>
2026-02-10 16:31:39 +0800

    added mongodb models on python side

--- python/app/core/mongo.py
+++ python/app/core/mongo.py
@@ -1,1 +1,14 @@
 #connection to mongodb
+import pymongo
+
+mongo_client = pymongo.MongoClient("mongodb://localhost:27017/")
+
+mydb = mongo_client["mydatabase"]
+mycol = mydb["customers"]
+
+myquery = { "address": "Park Lane 38" }
+
+mydoc = mycol.find(myquery)
+
+for x in mydoc:
+  print(x)
