fea9fb19c39d43825d1af701fa505b24b9c9783e
Javen420 <Javent2003@gmail.com>
2026-02-11 22:47:36 +0800

    added cors middleware to main.py to accept requests from frontend and express

--- python/app/main.py
+++ python/app/main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.api import stocks, grades
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173", "http://localhost:5000"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(stocks.router, prefix="/stocks")
 app.include_router(grades.router, prefix="/grades")
