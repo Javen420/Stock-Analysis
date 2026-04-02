93fc057889f4c1f0d683fc0ae35b7769dd73d4a5
Javen420 <Javent2003@gmail.com>
2026-04-02 14:49:53 +0800

    added docker

--- python/app/main.py
+++ python/app/main.py
@@ -7,12 +7,12 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173", "http://localhost:5174"],
     allow_methods=["GET", "POST"],
     allow_headers=["Content-Type", "Authorization"],
 )
 
-app.include_router(stocks.router, prefix="/stocks", dependencies=[Depends(require_auth)])
+app.include_router(stocks.router, prefix="/stocks")
 app.include_router(grades.router, prefix="/grades", dependencies=[Depends(require_auth)])
 
 
